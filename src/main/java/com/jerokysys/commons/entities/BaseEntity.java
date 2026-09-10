f4c6721fd9882afe4c2512e7298a5055532283e7
Adán Alvarez <geroshima123@gmail.com>
2026-09-10 16:25:17 -0300

    init

---
+++ src/main/java/com/jerokysys/commons/entities/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.jerokysys.commons.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
